19b0cf5df5213449942293f185151e3d9b34b5a5
Laura Damaceno de Almeida <41930302+lauraDamacenoAlmeida@users.noreply.github.com>
2019-09-18 21:55:47 -0300

    Add files via upload

---
+++ APS/app/src/main/java/br/com/aps/MainActivity.java
@@ -0,0 +1,43 @@
+package br.com.aps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String KEY_INFO_TEXTO = "KEY_INFO_TEXTO";
+    private Button botao_Cadastrar;
+    private Button botao_Consultar;
+    private EditText texto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        botao_Cadastrar = (Button) findViewById(R.id.btnCadastrar);
+        botao_Consultar = (Button) findViewById(R.id.btnConsultar);
+
+
+        botao_Cadastrar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent it = new Intent(MainActivity.this, TelaCadastrar.class);
+
+                startActivity(it);
+            }
+        });
+
+        botao_Consultar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent it = new Intent(MainActivity.this, TelaConsultar.class);
+
+                startActivity(it);
+            }
+        });
+    }
+}
