16870cee720ae54dcf0b10b220840e59f09f1e8d
Laura Damaceno de Almeida <41930302+lauraDamacenoAlmeida@users.noreply.github.com>
2019-09-30 15:35:33 -0300

    Update BancoController.java

--- APS/app/src/main/java/br/com/aps/BancoController.java
+++ APS/app/src/main/java/br/com/aps/BancoController.java
@@ -5,9 +5,11 @@ import android.database.sqlite.SQLiteDatabase;
 public class BancoController {
     private SQLiteDatabase db;
     private CriarBanco banco;
+    //Construtor da controlle do Banco de dados, onde assim que a classe BancoController é chamada é criado automaticamente uma instancia do banco.
     public BancoController(Context context){
         banco = new CriarBanco(context);
     }
+    // Classe para realizar a inserção dos dados que estão na tela de cadastro
     public String insereDado(String nome, String endereco, String celular,String email,String cpf, String data_nascimento,String categoria_leitor){
         ContentValues valores;
         long resultado;
